8b3553f9bf40aef54e18c2acf30835d000036ffd
theWhiteFox <ste@thewhitefox.ninja>
2018-06-14 20:39:32 +0100

    :bug: chapter 4 :tv:

--- quiz/main.js
+++ quiz/main.js
@@ -1,42 +1,17 @@
-const question = "What is Ashy Slashy's real name?";
-
-const hash = new Map();
-
-const map1 = new Map([["fish", "Mackerel"],["country", "Jamaica"]]);
-
-
-let mapIter = map1.values();
-
-console.log(mapIter.next().value)
-
-console.log(map1.size);
-console.log(map1.values());
-
-for (const dinner of map1.keys()) { 
-  console.log(dinner);
+const square = (x) => {
+  return  x * x;
 }
 
-let list = document.getElementById('demo');
+const numerically = (a,b) => {
+  return a - b;
+}
 
-map1.forEach((value, key, array) => {
-  let items = `<li>${key}: ${value}</li>`;
-  list.innerHTML = list.innerHTML + items;
-});
+let result = [4,3,5,1].map(square);
 
+result.forEach(element => {
+  console.log(element);
+})
 
-const n = 5;
-n % 2 === 0 ? console.log("even") : console.log("odd");
+console.log(square(5));
 
-const keyObject = {};
-
-let user = {     // an object
-    name: "John",  // by key "name" store value "John"
-    age: 30        // by key "age" store value 30
-  };
-
-  console.log(user);
-  console.log(`this is map1 ${map1}`);
-
-hash.set(1, 'I');
-hash.set(keyObject);
-hash.set(user);
+console.log(result);
